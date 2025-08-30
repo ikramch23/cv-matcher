f0e18ca2382c3860d0b7fa793feac7736f17840f
ikramch23 <ikramchafai3@gmail.com>
2025-08-30 23:09:47 +0300

    Wire root URL to matcher and add home view

--- matcher/views.py
+++ matcher/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("""
+    <html>
+      <head><title>CV-Matcher</title></head>
+      <body style="font-family: system-ui; padding:24px">
+        <h1>✅ CV-Matcher is live</h1>
+        <p>Your PythonAnywhere deployment is working.</p>
+        <p><a href="/admin/">Go to Django admin</a></p>
+      </body>
+    </html>
+    """)
