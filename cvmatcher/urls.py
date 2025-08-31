bdf232783339f3024ff0b863be81e19d5f5dc3fd
ikramch23 <ikramchafai3@gmail.com>
2025-08-31 13:42:56 +0300

    Wire matcher URLs into project and render home.html

--- cvmatcher/urls.py
+++ cvmatcher/urls.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("matcher.urls")),           # landing page/form
-    path("ik-admin-7c5b6167/", admin.site.urls) # optional hardened admin path
+    path("", include("matcher.urls")),
+    path("admin/", admin.site.urls),
 ]
