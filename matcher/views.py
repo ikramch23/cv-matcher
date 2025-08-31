cb0ea35cad99b2d71ab1d79079dc39fb78105daf
ikramch23 <ikramchafai3@gmail.com>
2025-08-31 15:22:33 +0300

    Add static folder with css/js

--- matcher/views.py
+++ matcher/views.py
@@ -1,4 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+def health(request):
+    return HttpResponse("OK from Django")
+
 def home(request):
-    return render(request, "home.html")
+    return render(request, "home.html")  # your template at matcher/templates/home.html
